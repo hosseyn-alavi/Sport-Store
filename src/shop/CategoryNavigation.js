7782a0f312ca4399de5e9b46a07e452504c01f65
s.m.hosseyn.alavi <s.m.gosseyn.alavi@gmail.com>
2019-04-14 14:31:47 +0430

    .

--- src/shop/CategoryNavigation.js
+++ src/shop/CategoryNavigation.js
@@ -5,12 +5,13 @@ export class CategoryNavigation extends Component {
     render() {
         return <React.Fragment>
             <Link className="btn btn-secondary btn-block"
-                to={ this.props.baseUrl }>All</Link>
-            { this.props.categories && this.props.categories.map(cat =>
-                <Link className="btn btn-secondary btn-block" key={ cat }
-                    to={ `${this.props.baseUrl}/${cat.toLowerCase()}`}>
-                    { cat }
+                to={this.props.baseUrl}>All</Link>
+            {this.props.categories && this.props.categories.map(cat =>
+                <Link className="btn btn-secondary btn-block" key={cat}
+                    to={`${this.props.baseUrl}/${cat.toLowerCase()}`}>
+                    {cat}
                 </Link>
             )}
         </React.Fragment>
-} }
+    }
+}
